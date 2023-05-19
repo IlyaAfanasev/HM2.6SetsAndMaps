b323097afa014c3ee3c46ab6f13bf610c26478a1
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-19 23:29:01 +0400

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 8, 2, 3, 4, 4, 5, 5, 6, 7));
+
         List<String> words= new ArrayList<>(List.of(
                 "Повторюсь", "в","отраженьях", "зеркал",
                 "Повторюсь", "в", "эхе", "собственных", "мыслей",
                         "Повторюсь",  "в", "слове", "что", "вслух", "сказал",
                 "И", "в", "стихах", "став", "их", "сути", "частицей"
         ));
+
+        List<String> strings = new ArrayList<>(List.of("один",
+                "два","два", "три",
+                "три", "три"));
+
         printOddNumbers(nums);
         printEvenSortNumbers(nums);
         printWords(words);
+        printWordsDouble(strings);
 
 
 
@@ -50,6 +55,22 @@ public class Main {
 
         System.out.println(words2);
 
+    }
+
+    public static void printWordsDouble(List<String> strings) {
+        Integer count=0;
+        Map<String, Integer> stringsMap=new HashMap<>();
+        for (String string : strings) {
+            for (int i = 0; i < strings.size(); i++) {
+                if (string ==strings.get(i)) {
+                    count++;
+
+                }
+                stringsMap.put(string, count);
+            }
+            count=0;
+        }
+        System.out.println(stringsMap);
     }
 
 }
