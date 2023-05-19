10909cae68232b63c525fd6fb710f8e417d3fd34
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-19 22:36:07 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        printOddNumbers(nums);
+
+
+
     }
+
+    public static void printOddNumbers(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+
 }
