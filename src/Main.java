4bb2946c81aac0e433b400dfdccb12032600dd9d
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-19 23:02:19 +0400

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 8, 2, 3, 4, 4, 5, 5, 6, 7));
         printOddNumbers(nums);
+        printEvenSortNumbers(nums);
 
 
 
@@ -18,5 +21,17 @@ public class Main {
         }
     }
 
+    public static void printEvenSortNumbers(List<Integer> nums) {
+
+        List<Integer> nums2=new ArrayList<>();
+        for (Integer num : nums) {
+            if (num % 2 == 0&&!nums2.contains(num)) {
+                nums2.add(num);
+            }
+        }
+        Collections.sort(nums2);
+        System.out.println(nums2);
+
+    }
 
 }
